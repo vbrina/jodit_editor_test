b6e31553bf513d9fb94b79c5fa2d551ebaab1bd0
Vitor Brina <vitor.brina@venturus.org.br>
2021-07-07 10:18:39 -0300

    Added new features.

--- src/JoditTest.js
+++ src/JoditTest.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import 'jodit';
-import 'jodit/build/jodit.min.css';
+import React from "react";
+import "jodit";
+import "jodit/build/jodit.min.css";
 import JoditEditor from "jodit-react";
 
-import './JoditTest.css';
+import "./JoditTest.css";
 
 export const JoditTest = () => {
+  const editor = React.useRef(null);
+  const editorRead = React.useRef(null);
+  const [content, setContent] = React.useState("");
+
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+    toolbar: false,
+    showCharsCounter: false,
+    showWordsCounter: false,
+    showXPathInStatusbar: false,
+    placeholder: 'Digite a descrição do anúncio...'
+  };
+
+  const configRead = {
+    readonly: true, // all options from https://xdsoft.net/jodit/doc/
+    toolbar: false,
+    showCharsCounter: false,
+    showWordsCounter: false,
+    showXPathInStatusbar: false,
+  };
+
+  // const content =
+  //   "<p>asdasdasdasdadasd</p><p><br></p><p>poisadjrepoisjdopisjropsijrposirjposdijrpsoidjrds</p><p><br></p><p>KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK</p>";
+
   return (
     <div className="JoditTest">
       <h1 className="h1">JODIT EDITOR TESTING</h1>
       <JoditEditor
-        config={{
-          toolbar: {toolbarInline: false},
-          autofocus: true,
-          uploader: {insertImageAsBase64URI: true},
-          spellcheck: false,
-          defaultMode: "1",
-          showCharsCounter: false,
-          showWordsCounter: false,
-          showXPathInStatusbar: false,
-          buttons: "paragraph,align,|,bold,,italic,underline,hr,|,image,source,table,ul,ol,link",
-        }}
+        ref={editor}
+        value={content}
+        config={config}
+        tabIndex={1}
+        onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+        onChange={() => {}}
+      />
+      <br />
+      <br />
+      <JoditEditor
+        ref={editorRead}
+        value={content}
+        config={configRead}
+        tabIndex={1}
       />
     </div>
   );
-}
+};
